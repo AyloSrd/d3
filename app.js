f1b029bf91d6919e1f7191d24813591f3745c760
AyloSrd <aylo.srd@gmail.com>
2020-06-28 16:29:36 +0200

    k

--- app.js
+++ app.js
@@ -1,62 +1,64 @@
-const data = [
-  {},
-  {}
-]
-const createStatistic = (str) => {
-  let chrs = str.split('').sort()
-  let sentenceData = []
-  
-  let character = '' 
-  let frequency = 1
+d3.select("#reset")
+    .on("click", function() {
+      d3.selectAll(".letter")
+        .remove();
 
-  for (let i = 0; i < chrs.length; i++) {
-    character = chrs[i]
-    if (chrs[i] === chrs[i + 1]) {
-      frequency++
-    }else{
-      sentenceData.push([character, frequency])
-      character = chrs[i]
-      frequency = 1
-    }
-  }
-  return sentenceData
-} 
-d3.select("form")
-    .on(
-      'submit', 
-      () => {
-      d3.event.preventDefault();
-      var input = d3.select('input');
-      data.shift()
-      data.push(createStatistic(input.property('value')))
-      console.log(data)
+      d3.select("#phrase")
+          .text("");
+
+      d3.select("#count")
+          .text("");
     });
-/*
-d3.select("#remove-notes-btn")
-      .on('click', function() {
-    d3.selectAll(".note")
-      .remove()
-});
 
-d3.select("#feeling-lucky-btn")
-    .on('click', () => {
-  d3.selectAll(".note")
-      .style(
-        'font-size',
-        () => `${Math.random()*50 + 10}px`
-      )
-});
+d3.select("form")
+    .on("submit", function() {
+      d3.event.preventDefault();
+      var input = d3.select("input");
+      var text = input.property("value");
 
+      var letters = d3.select("#letters")
+                      .selectAll(".letter")
+                      .data(getFrequencies(text), function(d) {
+                        return d.character;
+                      });
 
-d3.select("input")
-    .on(
-      'input',
-      () => {
-        let tempTxt = d3.event.target.value //or d3.select(d3.event.target).property('value')
-        d3.select('#preview')
-            .style('display', 'block')
-            .text(tempTxt)
-        
-        tempTxt === '' && d3.select('#preview').style('display', 'none')
-      }
-      )*/
+      letters
+          .classed("new", false)
+        .exit()
+        .remove();
+
+      letters
+        .enter()
+        .append("div")
+          .classed("letter", true)
+          .classed("new", true)
+        .merge(letters)
+          .style("width", "20px")
+          .style("line-height", "20px")
+          .style("margin-right", "5px")
+          .style("height", function(d) {
+            return d.count * 20 + "px";
+          })
+          .text(function(d) {
+            return d.character;
+          });
+
+      d3.select("#phrase")
+          .text("Analysis of: " + text);
+
+      d3.select("#count")
+          .text("(New characters: " + letters.enter().nodes().length + ")");
+
+      input.property("value", "");
+    });
+
+function getFrequencies(str) {
+  var sorted = str.split("").sort();
+  var data = [];
+  for (var i = 0; i < sorted.length; i++) {
+    var last = data[data.length - 1];
+    if (last && last.character === sorted[i]) last.count++;
+    else data.push({ character: sorted[i], count: 1 });
+  }
+  return data;
+}
